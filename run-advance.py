6859ba8c112df22a98c4e42523711af58c51fe80
AL SP1IT SAYA <128552610+musu10gj@users.noreply.github.com>
2023-06-20 01:52:04 +0500

    Create run-advance.py

---
+++ run-advance.py
@@ -0,0 +1,15 @@
+import os
+
+os.system("pkg install nano")
+
+os.system("pkg install cmatrix")
+
+os.system("pkg install hollywood")
+
+os.system("pkg install toilet")
+
+os.system("pkg update")
+
+os.system("pkg upgrade")
+
+print("advance done")
