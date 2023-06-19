bdc8324c2c77d506bd55760f1ee0560659a0ff7d
AL SP1IT SAYA <128552610+musu10gj@users.noreply.github.com>
2023-06-20 01:50:46 +0500

    Create run-basics.py

---
+++ run-basics.py
@@ -0,0 +1,15 @@
+import os
+
+os.system("apt update")
+
+os.system("apt upgrade")
+
+os.system("pkg install python")
+
+os.system("pkg install python2")
+
+os.system("pkg install python3")
+
+os.system("pkg install git")
+
+print("basics done")
