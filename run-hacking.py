440878bb538a598645ccf6d5f4e04382db00bcd0
AL SP1IT SAYA <128552610+musu10gj@users.noreply.github.com>
2023-06-20 01:53:39 +0500

    Create run-hacking.py

---
+++ run-hacking.py
@@ -0,0 +1,37 @@
+import os
+
+os.system("pkg install nmap")
+
+print ("nmap installed")
+
+print ("running more")
+
+print ("5")
+
+print ("4")
+
+print ("3")
+
+print ("2")
+
+print ("1")
+
+print ("run")
+
+print ("installing all hacking tool")
+
+os.system("apt update")
+
+os.system("apt upgrade")
+
+os.system("termux-setup-storage")
+
+("apt install git")
+
+os.system("git clone https://github.com/mishakorzik/AllHackingTools")
+
+os.system("cd AllHackingTools")
+
+os.system("clear")
+
+os.system("bash Install.sh")
